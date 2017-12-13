3fd8fcd2c5052db00b018dadf315185b88c10bb2
minikin <djminikin@gmail.com>
2017-12-13 11:24:13 +0100

    Initial commit

---
+++ Sources/ItemsDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  ItemsDataSource.swift
+//  ItemsDataSource
+//
+//  Created by Sasha Prokhorenko on 13.12.17.
+//  Copyright © 2017 ItemsDataSource. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/ItemsDataSourceTests/ItemsDataSourceTests.swift
@@ -0,0 +1,23 @@
+//
+//  ItemsDataSourceTests.swift
+//  ItemsDataSource
+//
+//  Created by Sasha Prokhorenko on 13.12.17.
+//  Copyright © 2017 ItemsDataSource. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import ItemsDataSource
+
+class ItemsDataSourceTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(ItemsDataSource().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
